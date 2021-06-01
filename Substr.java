510e2b546251663a4cb1aaf761ca1168f14ac111
Prakash Thakur <prthakur171@gmail.com>
2021-06-01 22:17:10 +0530

    Substr commit

---
+++ Substr.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Substr{
+	public static void main(String args[]){
+		Scanner o=new Scanner(System.in);
+		String s=o.nextLine();
+		System.out.println(s.substring(1,5));
+	}
+}
+
