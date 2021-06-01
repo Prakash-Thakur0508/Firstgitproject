0d031844c4e736adcb61904fedd384f78b91a012
Prakash Thakur <prthakur171@gmail.com>
2021-06-01 23:07:48 +0530

    Editing for Sum.java done

--- Sum.java
+++ Sum.java
@@ -1,8 +1,6 @@
 public class Main{
 	public static void main(String args[]){
-		int a=100;
-		int b=200;
-		int c=a+b;
-		System.out.println("The sum of a and b is: "+c);
+		
+		System.out.println("The sum of a and b is: "+100);
 	}
 i}
